372193e47fddc1defd33e888b8c0f74e98d80e6d
Tsarina <tsarina.islam@gmail.com>
2019-05-18 20:55:57 -0400

    shoulda done this sooner

---
+++ assets/javascript/game.js
@@ -0,0 +1,28 @@
+// Variables
+var crystal = {
+    blueCryst: {
+        value: 0,
+    },
+    greenCryst: {
+        value: 0,
+    },
+    PurpCryst: {
+        value: 0,
+    },
+    yellowCryst: {
+        value: 0,
+    }
+}
+
+
+var randomNumber = 0
+var yourScore = 0
+
+var wins
+var losses
+
+
+// Functions
+
+
+// Process
